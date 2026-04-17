205954595f7e39be2d7df62c7f07e9c3e25a9e96
Zeus-crtl <argeiphonteargus@gmail.com>
2026-04-17 20:21:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "opinfo-v1";
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                "./",
+                "/index.html"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
